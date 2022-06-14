dac93ad1207597a9c6c90259a8f35afab20ae3f0
LukeSlater02 <lukeslater101@gmail.com>
2022-06-14 16:00:25 -0500

    added options for color schemes - actually reading the instructions is cool

--- TabloidCLI/Program.cs
+++ TabloidCLI/Program.cs
@@ -7,14 +7,44 @@ namespace TabloidCLI
     {
         static void Main(string[] args)
         {
+            
+            Console.WriteLine("Would you like to change the console color scheme? Or just press enter to keep the standard scheme.");
+            Console.WriteLine("1. Ocean Waves");
+            Console.WriteLine("2. Vampire");
+            Console.WriteLine("3. Bumblebee");
+            string colorSelection = Console.ReadLine();
 
+            var backgroundColor = ConsoleColor.Black;
+            var textColor = ConsoleColor.Gray;
+
+            switch (colorSelection)
+            {
+                case "1":
+                    backgroundColor = ConsoleColor.DarkCyan;
+                    textColor = ConsoleColor.White;
+                    break;
+                case "2":
+                    backgroundColor = ConsoleColor.DarkRed;
+                    textColor = ConsoleColor.Black;
+                    break;
+                case "3":
+                    backgroundColor = ConsoleColor.Yellow;
+                    textColor = ConsoleColor.Black;
+                    break;
+                default:
+                    break;
+            }
             // MainMenuManager implements the IUserInterfaceManager interface
             IUserInterfaceManager ui = new MainMenuManager();
             while (ui != null)
             {
-                Console.BackgroundColor = ConsoleColor.DarkCyan;
-                Console.ForegroundColor = ConsoleColor.White;
+
+                Console.BackgroundColor = backgroundColor;
+                Console.ForegroundColor = textColor;
                 Console.Clear();
+
+                Console.WriteLine("Pleasant Greeting!");
+                Console.WriteLine("------------------");
                 // Each call to Execute will return the next IUserInterfaceManager we should execute
                 // When it returns null, we should exit the program;
                 ui = ui.Execute();
