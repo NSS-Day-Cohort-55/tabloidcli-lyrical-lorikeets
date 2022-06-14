771ed7d46a7340364e2be79c9e5d3315c4b35e03
acheatham87 <acheatham87@gmail.com>
2022-06-14 11:57:21 -0500

    build blog repository and manager cs, edited main menu to accept blog input, currently displaying all blogs

---
+++ TabloidCLI/Repositories/BlogRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using TabloidCLI.Models;
+using TabloidCLI.Repositories;
+
+namespace TabloidCLI
+{
+    public class BlogRepository :DatabaseConnector, IRepository<Blog>
+    {
+        public BlogRepository(string connectionString) : base (connectionString) { }
+
+        public List<Blog> GetAll()
+        {
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                using (SqlCommand cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = @"SELECT Id, Title, URL
+                                        FROM Blog";
+
+                    List<Blog> blogs = new List<Blog>();
+
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Blog blog = new Blog()
+                        {
+                            Id = reader.GetInt32(reader.GetOrdinal("Id")),
+                            Title = reader.GetString(reader.GetOrdinal("Title")),
+                            Url = reader.GetString(reader.GetOrdinal("URL")),
+                        };
+                        blogs.Add(blog);
+                    }
+
+                    return blogs;
+                }
+            }
+        }
+
+        public Blog Get(int id)
+        {
+            Blog blog = null;
+            return blog;
+        }
+
+        public void Insert(Blog entry)
+        {
+
+        }
+
+        public void Update(Blog entry)
+        {
+
+        }
+
+        public void Delete(int id)
+        {
+
+        }
+    }
+}
